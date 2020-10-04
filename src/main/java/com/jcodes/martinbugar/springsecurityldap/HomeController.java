5ecc6b7e372007a692e6f2f87588627f4f375548
Martin <martin1.bugar@gmail.com>
2020-10-04 11:44:30 +0200

    Done

--- src/main/java/com/jcodes/martinbugar/springsecurityldap/HomeController.java
+++ src/main/java/com/jcodes/martinbugar/springsecurityldap/HomeController.java
@@ -1,4 +1,14 @@
 package com.jcodes.martinbugar.springsecurityldap;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HomeController {
+
+    @GetMapping("/")
+    public String index (){
+        return "Home page";
+    }
+
 }
